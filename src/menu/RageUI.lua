c44b942c1c770b7fc003ce81e84c4174b0a3203d
x2zHs0 <76494889+x2zHs0@users.noreply.github.com>
2021-01-02 15:49:18 +0100

    Update RageUI.lua

--- src/menu/RageUI.lua
+++ src/menu/RageUI.lua
@@ -3,10 +3,9 @@
 --- Created by Dylan Malandain.
 --- DateTime: 21/04/2019 21:20
 ---
-
 print("")
-print("^1Crée par : Haci Petlay^0")
-print("^4Pour : LegendRP.^0")
+print("^1Crée par : Huso^0")
+print("^4Discord : https://discord.gg/9yszV9tDJw^0")
 print("")
 
 
